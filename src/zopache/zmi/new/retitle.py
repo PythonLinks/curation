def29cbb4d2f9299dab781c3860289916ebe5e34
Christopher Lozinski <lozinski@PythonLinks.info>
2018-11-10 12:59:51 +0000

    Changed Permissions on the retitle / manage view objects. No permission required but it does use the baseedit permissions on a branch now.

--- src/zopache/zmi/new/retitle.py
+++ src/zopache/zmi/new/retitle.py
@@ -14,16 +14,16 @@ from zopache.application.interfaces import ITab
 from cromlech.security import permissions
 from zopache.zmi.interfaces import IURLSegment
 from zopache.zmi.interfaces import IObjectRetitler
+from zopache.categories.baseedit import BaseEdit
 
 @view_component
 @name('manage')
-@title("Edit Titles")
+@title("Manage")
 @target(ITab)
-@permissions('Manage')
 @context(IBTreeContainer)
-class Manage(Page,Contents):
+class Manage(BaseEdit,Page,Contents):
     label=''
-    subTitle='Rename Videos '
+    subTitle='Rename Videos. Cut and Paste them.  '
     # TEMPLATE IS IN THE ZODB
     
     def getRoot(self):
