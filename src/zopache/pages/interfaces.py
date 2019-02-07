59e5baf5e4bb0b19bd06f57f7594777210a6ede1
Christopher Lozinski <lozinski@PythonLinks.info>
2019-02-07 19:02:08 +0000

    asically added a map and location to pages.

--- src/zopache/pages/interfaces.py
+++ src/zopache/pages/interfaces.py
@@ -4,7 +4,7 @@ from zopache.crud.interfaces import IContainer
 from dolmen.container import IBTreeContainer
 from cromlech.container.interfaces import IOrdered
 from cromlech.browser.interfaces import IPublicationRoot
-
+from .geo import Address
 from zopache.ttw.interfaces import IUntrustedHTML
 
 class IPage(IContainer,IOrdered ,IUntrustedHTML):
@@ -42,3 +42,59 @@ class IRootPage(IPublicationRoot,IPage):
 
 class INotPage (Interface):
      pass
+
+
+    
+class ILocation(IPage):
+    lattitude = schema.Float(
+        title = u'Lattitude',
+        description = u'Lattitude',
+        min=-90.,
+        max=90.,
+        default = 45.,
+        required = True,
+    )
+
+    longitude = schema.Float(
+        title = u'Longitude',
+        description = u'Longitude ',
+        min=-180.,
+        max=180.,
+        default = 0.,
+        required = True,
+    )
+
+class IMap(ILocation):
+    zoomLevel = schema.Float(
+        title = u'Google Maps Zoom Level',
+        description = u'Google Maps Zoom Level',
+        min=0.,
+        max=22.,
+        default = 5., 
+        required = True,
+    )
+    """
+    mapWidth = schema.Float(
+        title = u'Map Width',
+        description = u'Map Width ',
+        min=-0.,
+        max=20000.,
+        required = True,
+    )
+    
+    mapHeight = schema.Float(
+        title = u'mapHeight',
+        description = u'mapHeight ',
+        min=0.,
+        max=20000.,
+        required = True,
+    )    
+   
+    address= Address(
+        title = u'Address (For the map)',
+        description = u'Where is the main office for this company?',
+        required = True,
+        default = u'',
+    )
+    """
+ 
---
+++ src/zopache/pages/location.py
@@ -0,0 +1,99 @@
+from .interfaces import ILocation, IMap
+from .page import Page
+from zopache.pages.interfaces import IPage , IRootPage
+from zope.interface import implementer
+from .geo import geoCache
+
+@implementer (ILocation)
+class Location (Page):
+    lattitude = 45.
+    longintude = 0.
+    webClass = 'Location'
+
+    def postProcess(self):
+          #geoCache.geoCode(self.context.address)
+          pass
+
+  
+    #JUST ADD ONE MARKER TO THE LIST                        
+    def getOneMarker(self, firstItem, result):
+                  if not hasattr(self, 'longitude'):
+                      return result,firstItem
+                  if not firstItem:
+                     result +=','
+                  firstItem=False      
+                  result+='\n'
+                  result += u'['
+                  result +='\'' +  self.__name__ + '\''
+                  result += ','    
+                  result +=  str(self.lattitude).decode('utf8')  
+                  result += u','    
+                  result += str(self.longitude).decode('utf8')
+                  result += ']'
+                  return result, firstItem
+
+import googlemaps            
+class Map(Location):
+    zoomLevel=5.
+    mapHeight=0.
+    mapWidth=0.
+    webClass = 'Map'
+
+      
+    # GET THE JSON FOR CHILD LOCATIONS
+    def getLocationsJSON(self):
+        firstItem=True
+        result=''             
+        begin= 'var locations =['
+        end='\n];'
+        result, firstItem= self.getLocationsRecursively(
+                                firstItem,result)
+        return begin + result + end
+
+
+    def getLocationsRecursively(self,firstItem,result):
+
+        for item in self.values():
+             if not ILocation.providedBy(item):
+                   continue
+
+             # IF LOCATION GET THE JSON
+             if ( item.webClass=='Location'):
+                result, firstItem= item.getOneMarker(firstItem,result)
+
+            #IF IF IS A MAP SHOW IT
+            #OR SHOW A SINGLETON CHILD
+             if ( item.weClass=='Map'): 
+                  location=item.onlyOneLocationIn()
+                  if (location!=None):
+                     item = location
+                  result, firstItem= item.getOneMarker(  
+                            firstItem,result)
+
+        return result , firstItem
+
+
+    #ITERATE THROUGH THE CHILDREN
+    # IF ONLY ONE COMPANY RETURN IT, ELSE RETURN NONE
+    def onlyOneLocatinIn(self):
+         company=None
+         for item in self.values():
+             if ILocation.providedBy(item):
+                    if (company!=None):
+                          return None
+                    company=item
+         return company          
+    
+
+    #NOT USED, BUT POTENTIALLY USEFUL
+    def getLocations(self):
+        values=self.values()
+        result=[]
+        for item in values:
+            if (ILocation.providedBy(item) and 
+                item.webApproved):
+                result.append(item)
+        return result
+
+    
+        
