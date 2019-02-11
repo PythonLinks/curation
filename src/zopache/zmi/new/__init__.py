289a9c1a7cf584bf72f4831535375f2b7f44d79a
Christopher Lozinski <lozinski@PythonLinks.info>
2019-02-11 18:09:18 +0000

    Mostly this is to get zopache working with the wiki. It now does Moved zmi/new to zopache.categories.

--- src/zopache/zmi/new/__init__.py
+++
@@ -1,8 +0,0 @@
-#just to make sure this is included
-from dolmen.template import TALTemplate
-from os import path
-TEMPLATE_DIR = path.dirname(__file__)
-def tal_template(name):
-    name =  TALTemplate(path.join(TEMPLATE_DIR, name))    
-    print ("TEMPLATE = ", path)
-    return path
