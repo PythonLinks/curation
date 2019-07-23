790a6f8b99d855fe390436dc29d7e670a39a69bf
Christopher Lozinski <lozinski@PythonLinks.info>
2019-07-23 18:26:23 +0000

    One can now edit the web class of an object. Sadly does not yet give a tree of classes to choose from.

---
+++ src/zopache/business/editwebclass.py
@@ -0,0 +1,35 @@
+import googlemaps
+
+from zope.interface import Interface
+from zope.schema._field import Choice
+from zope.schema import TextLine
+
+from cromlech.browser.exceptions import HTTPFound
+
+from zopache.core.viewdecorators import *
+from zopache.crud.forms import EditForm
+from zopache.pages.interfaces import IPage
+
+class IClass(Interface):
+
+    webClass= TextLine(
+        title = u'WebClass.',
+        description = u'Defines the source of html, css, javascript aand images',
+        required = True,
+        default = u'',
+    )         
+
+@form_component
+@name ('editWebClass')
+@context(IPage)
+class EditWebClass (EditForm):
+    title = 'Edit the WebClass.'
+    subTitle = 'This is used to change the layout of this object.'
+    interface = IClass
+    fields = Fields(IClass)
+    
+    
+    def acquireTitle(self):
+        return 'Edit' + self.context.title + "'s Web Class "
+
+
