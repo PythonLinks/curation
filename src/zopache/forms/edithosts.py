e4c939351fb58ad0b156b32e2bdace442ef4f1e5
Christopher Lozinski <lozinski@PythonLinks.info>
2020-03-21 12:45:51 +0000

    Added the file editVirtualHosts.py

---
+++ src/zopache/forms/edithosts.py
@@ -0,0 +1,36 @@
+from zope.interface import Interface
+from zope.schema.vocabulary import SimpleVocabulary
+from zope.schema import TextLine, Dict
+
+
+class IVirtualHosts(Interface):
+         
+    virtualHosts = Dict (
+        key_type = TextLine(),
+        value_type = TextLine(),
+        title="Virtual Hosts",
+        description= "Map from domain name to Folder.",
+        required = False)                
+    
+
+from zopache.core.viewdecorators import *
+from zopache.crud.interfaces import  IZodbRoot
+from zopache.crud.forms import EditForm
+
+@form_component
+@name ('editVirtualHosts')
+@context(IZodbRoot)
+@permissions('Manage')
+class EditPermissions (EditForm):
+    title = 'Edit The Virtual Hosts'
+    suTitle = 'Map from domain to Folder.'
+    interface = IVirtualHosts
+    fields = Fields(IVirtualHosts)
+        
+    def acquireTitle(self):
+        return 'Edit Permissions'
+    
+    def updateWidgets(self):
+        breakpoint()        
+        super().updateWidgets()
+        pass
