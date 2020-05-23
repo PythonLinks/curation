c673611c076123dde91ad333eec1c98ec5b3f919
Christopher Lozinski <lozinski@PythonLinks.info>
2020-05-23 18:58:17 +0000

    Added some missing files. AddAnonymous is the big new one. and ./application/choices.py

---
+++ src/zopache/application/choices.py
@@ -0,0 +1,23 @@
+from zope.schema.vocabulary import SimpleVocabulary
+
+def fromList(aList,includeNone=False):
+    terms = []
+    if includeNone == True:
+        term = SimpleVocabulary.createTerm('None','None','None')
+        terms.append(term)
+        
+    for item in aList:
+        term = SimpleVocabulary.createTerm(item,item,item)
+        terms.append(term)
+    return SimpleVocabulary(terms)
+
+def fromDict(choiceDict,includeNone=False):
+    terms = []
+    if includeNone:
+        term = SimpleVocabulary.createTerm('None','None','None')
+        terms.append(term)
+
+    for key,value in choiceDict.items():
+        term = SimpleVocabulary.createTerm(key,key,value)
+        terms.append(term)
+    return SimpleVocabulary(terms)
---
+++ src/zopache/pages/addanonymous.py
@@ -0,0 +1,42 @@
+from dolmen.forms.base import Actions
+from zopache.crud import actions as formactions
+from zopache.crud import actions as formactions
+from zopache.crud.actions import AddByTitle
+from zopache.pages.pageactions import AddAndView
+from zopache.crud.forms import AddByTitleToTree
+from zopache.pages.addpage import AddPageVeryBase
+from zopache.business.exists import Duplicate
+
+class AddToTreeAndView(AddByTitleToTree):
+    def newURL(self,baseURL):
+        return baseURL 
+
+class AddAnonymous(AddPageVeryBase):
+
+    count = 0 
+    layoutName = "UserMenu"
+    subTitle = "All submissions are reviewed before becoming being publicly visible."
+    allowAnonymous = True    
+    dataValidators = [Duplicate]
+    actions = Actions()    
+    def update(self):
+        self.actions = Actions(
+                  AddAndView("Add and View", self.factory),
+                  formactions.Cancel("Cancel","Cancel"))
+
+    def postAddProcess(self,view = None):
+        self.new.postAddProcess (view = self)
+        self.notifyAdminsNewPage()
+
+    def widgetJsonURL(self):
+        siteRoot = self.getSiteRoot()
+        mapName = siteRoot.mapName
+        uri ="https://" + self.getDomain() + "/" + mapName + "/json"
+        return uri
+        
+
+class AddAnonymousToTree(AddAnonymous):
+    def update(self):
+        self.actions = Actions(
+                  AddToTreeAndView("Add and View", self.factory),
+                  formactions.Cancel("Cancel","Cancel"))
