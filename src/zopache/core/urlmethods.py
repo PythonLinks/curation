cafdad43430a2c5e46177fdb31c77fc192743256
Christopher Lozinski <lozinski@PythonLinks.info>
2020-07-01 10:43:23 +0000

    Fixed the /root/root relative URL.

--- src/zopache/core/urlmethods.py
+++ src/zopache/core/urlmethods.py
@@ -97,10 +97,10 @@ class URLMethods(object):
             base_url = ""
             return base_url
         elif isZodbRoot:
-            base_url = item.__name__
+            base_url = ""
             return base_url
         if isSiteRoot:
-            base_url =  item.__name__
+            base_url =  ""
             return base_url            
         else:
             container = item.__parent__
