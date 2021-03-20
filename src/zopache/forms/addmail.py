4aee72ce1332e88e4fcc431a0e0f34448d7a8cd7
Christopher Lozinski <lozinski@PythonLinks.info>
2021-03-20 07:15:03 +0000

    Added in some existing files.

---
+++ src/zopache/forms/addmail.py
@@ -0,0 +1,37 @@
+from zopache.crud.forms import AddByNameForm , EditForm
+from zopache.core.viewdecorators import *
+from zopache.ttw.mail import MailHost
+from zopache.pages.interfaces import IPage
+from zopache.core.interfaces import ITreeSecurity
+from zopache.ttw.interfaces import IMailHost
+
+@form_component
+@name('addMailHost')
+@context(IPage)
+@implementer(ITreeSecurity)
+class AddMailHost(AddByNameForm):
+    subTitle='Add a MailHost'
+    interface = IMailHost
+    ignoreContent = True
+    factory=MailHost
+    def newName(self,data):
+        return "MailHost"
+    def newURL (self,baseURL):
+        return "./manage"
+
+    #HERE IS THE  EDIT FORM
+@form_component
+@context(IMailHost)
+@name("edit")
+@implementer(ITreeSecurity)
+class EditMailHost(EditForm):
+    subTitle='Edit the MailHost Object'    
+ 
+    """    
+    def sendOneNewsletter(self):
+        to = self.mailer.postMaster
+        articles = self.context.bestMostRecentPage()
+        self.createOneNewsLetter(to, self.sender, articles)
+        self.sendTheMail()
+    """
+
---
+++ src/zopache/python/iskulpt.py
@@ -0,0 +1,92 @@
+from zope import schema
+from zope.interface import implementer
+from zope.interface import Interface
+from zopache.ttw.interfaces import ICanonical
+from zopache.ttw.interfaces import ISourceLeaf
+from zopache.zmi.interfaces import IZMI
+from zopache.crud.interfaces import IDeletable
+
+class ISkulptSolution(ISourceLeaf,IZMI,IDeletable):
+    """Student solution data. """
+
+    comments= schema.Text(
+        title = 'Comments',
+        description = "Teacher <-> Student comments",
+        required = False,
+        default = u'',
+    )
+    
+    problemCode= schema.Text(
+        title = u'Solution Source Code',
+        description = "The student's answer.",
+        required = False,
+        default = u'',
+    )
+    
+
+class ISkulptAssignment(ICanonical):
+
+    title = schema.TextLine(
+        title = u'Title',
+        description = 'The Name of this assignment.',
+        default='',            
+        required = True,
+    )
+
+    problemText= schema.Text(
+        title = 'The problem statement.',
+        description = 'Please explain the problem.',
+        required = False,
+        default = u'',
+    )
+    
+    problemCode= schema.Text(
+        title = u'Starting Source Code',
+        description = 'Here is the code you give to the students.',
+        required = False,
+        default = u'',
+    )
+    
+    comments= schema.Text(
+        title = 'Comments',
+        description = "Notes between the student and teacher."  ,
+        required = False,
+        default = u'',
+    )
+    
+    solutionText= schema.Text(
+        title = u'Explanation of the solution.',
+        description = u'An English language explanation of the solution.',
+        required = False,
+        default = u'',
+    )
+    
+    solutionCode= schema.Text(
+        title = 'Solution Code',
+        description = 'A correct program which answers the problem.',
+        required = False,
+        default =
+        '',
+    )        
+
+#    correctAnswer= schema.Text(
+#        title = 'Correct Result',
+#        description = 'What the program should return, if left empty, not checked.',
+#        required = False,
+#        default =
+#        '',
+#    )    
+    
+    showSolution = schema.Bool(
+        title = 'Show Solution?',
+        description = "Show the solution already?",
+        required = False,
+        default = False,
+    )
+
+    timeIsUp = schema.Bool(
+        title = 'Time is up?',
+        description = "Prevent further answers when the time has run out.",
+        required = False,
+        default = False,
+    )    
