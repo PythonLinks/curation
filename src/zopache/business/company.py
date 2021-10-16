d5098aa8b0466af29634c83ab5884e7666cc5f78
Christopher Lozinski <lozinski@PythonLinks.info>
2021-10-16 11:10:10 +0000

    Okay, this time set title should work.

--- src/zopache/business/company.py
+++ src/zopache/business/company.py
@@ -33,9 +33,13 @@ class Base(Page):
         
     #NOT SURE WHY THIS HAS TO Be here.
     #REALLY IT SHOULD JUST BE IN ONLINEJSONPROPERTIES
-    @property
-    def title(self):
+    def getTitle(self):
         return self.json['content'][0]["title"]
+
+    def setTitle(self,value):
+        self.json['content'][0]["title"] = value
+        
+    title = property(getTitle,setTitle)    
     
     def getSpecialization(self):
         if hasattr(self,'specialization') and self.specialization != '':
