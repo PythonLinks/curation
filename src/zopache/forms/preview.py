be4fc4e379d18ea47028f52008b159923d5cafb0
Christopher Lozinski <lozinski@PythonLinks.info>
2022-01-05 16:15:10 +0000

    Preview now has local and remote preview. local preview directly accesses the data, and also returns parent slug and title. Remote Preview also returns the image data and content type.

--- src/zopache/forms/preview.py
+++ src/zopache/forms/preview.py
@@ -1,35 +1,33 @@
 import json
+import requests
+import base64
+from zope.interface import Interface
 
 from webpreview import web_preview
 from cromlech.webob.response import Response
 from dolmen.view import View
+from cromlech.browser.interfaces import IPublicationRoot
 
 from zopache.ttw.JSON import makeJsonResponse
 from zopache.core.viewdecorators import *
 from zopache.pages.interfaces import ISiteRootPage
+from zopache.core.breadcrumbs import Breadcrumbs
 
 @view_component
-
 @name('fetchUrl')
-@context(ISiteRootPage)
-class Index(View):
+@context(Interface)
+class Index(View,Breadcrumbs):
     responseFactory = Response
     make_response = makeJsonResponse
         
     def render(self):
         try:
             url = self.request.form ['url']
-            title, description, image = web_preview(url)
-            result ={"success": 1,
-                     "meta": {'title':title,
-                              'url':url,
-                      'description':description,
-                      'image':{
-                          'url':image}
-            }}
-            
-            result = json.dumps(result)
-            return result
+            if self.getDomain() in url.lower():
+                return self.localPreview(url)
+            else:
+                return self.remotePreview(url)
+
         except:
             result = {
                 "success" : 0,
@@ -38,3 +36,47 @@ class Index(View):
             }
             result = json.dumps(result)
             return result
+
+    def remotePreview(self,url):
+            title, description, image = web_preview(url)
+            result ={"success": 1,
+                     "meta": {'title':title,
+                              'url':url,
+                      'description':description,
+                      'image':{
+                          'url':image}
+            }}
+            try:
+               if image != None:
+                  response  = requests.get(image)
+                  image = result['meta']['image']
+                  if response.status_code == 200:
+                      mime = response.headers['Content-Type']
+                      data = response.content
+                      data = base64.b64encode(data).decode('utf-8')
+                      image['mime-type'] = mime
+                      image['image-data'] = data 
+            except:
+                pass
+            result = json.dumps(result)
+            return result
+        
+    def localPreview(self,url):
+         siteRoot = self.getSiteRoot()
+         slug = url.split('/')[-1]
+         item = siteRoot[slug]
+         parent = item.parent
+         
+         result ={"success": 1,
+                     "meta": {'title':item.title,
+                              'url':url,
+                              'description':item.description,
+                              'parentSlug' : parent.name,
+                              'parentTitle': parent.title,       
+                              'image':{
+                                  'url':url + '/Logo150W'}
+                              
+            }}
+            
+         result = json.dumps(result)
+         return result
